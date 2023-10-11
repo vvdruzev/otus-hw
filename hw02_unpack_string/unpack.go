96c2bf58fe81900fbd4d428f8e3a65d6ae499ee2
Друзев Василий Викторович <vvdruze1@mts.ru>
2023-10-11 17:20:18 +0300

    HW1 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,56 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	end := len(str)
+	if end == 0 {
+		return "", nil
+	}
+	_, err := strconv.Atoi(str[0:1])
+	if err == nil {
+		return "", fmt.Errorf("некорректная строка")
+	}
+	if end == 1 {
+		return str, nil
+	}
+
+	str2 := strings.Split(str, "")
+
+	var result string
+	var c string
+	end = len(str2)
+
+	for i := 0; i < end-1; i++ {
+		c = c + str2[i]
+		if i <= end-2 {
+
+			if _, err = strconv.Atoi(str2[i] + str2[i+1]); err == nil {
+				return "", fmt.Errorf("некорректная строка")
+			}
+		}
+		number, err := strconv.Atoi(str2[i+1])
+		if err != nil {
+			result = result + c
+			c = ""
+			if i == end-2 {
+				result = result + str2[i+1]
+			}
+			continue
+		}
+		result = result + strings.Repeat(c, number)
+		c = ""
+		i++
+		if i == end-2 {
+			result = result + str2[i+1]
+		}
+
+	}
+	return result, nil
 }
