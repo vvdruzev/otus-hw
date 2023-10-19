9a4ace3bcd56c45a34e4b13666c593eda9b91e86
Друзев Василий Викторович <vvdruze1@mts.ru>
2023-10-19 11:07:52 +0300

    HW2 is completed. fix. добавил вторую функцию unpack2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -4,9 +4,45 @@ import (
 	"errors"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
+
+func Unpack2(str string) (string, error) {
+	var result string
+	runes := []rune(str)
+	if len(runes) == 0 {
+		return result, nil
+	}
+	remember := runes[0]
+
+	if unicode.IsDigit(remember) {
+		return result, ErrInvalidString
+	}
+
+	for i := 1; i < len(runes); i++ {
+		current := runes[i]
+		if unicode.IsDigit(current) {
+			if unicode.IsDigit(remember) {
+				return "", ErrInvalidString
+			}
+			number, _ := strconv.Atoi(string(current))
+			result += strings.Repeat(string(remember), number)
+			remember = current
+			continue
+		}
+		if !unicode.IsDigit(remember) {
+			result += string(remember)
+		}
+
+		remember = current
+	}
+	if !unicode.IsDigit(remember) {
+		result += string(remember)
+	}
+	return result, nil
+}
 
 func Unpack(str string) (string, error) {
 	end := len(str)
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,8 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "a", expected: "a"},
+		{input: "汉汉汉0b", expected: "汉汉b"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -26,7 +28,7 @@ func TestUnpack(t *testing.T) {
 	for _, tc := range tests {
 		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
-			result, err := Unpack(tc.input)
+			result, err := Unpack2(tc.input)
 			require.NoError(t, err)
 			require.Equal(t, tc.expected, result)
 		})
@@ -38,7 +40,7 @@ func TestUnpackInvalidString(t *testing.T) {
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
-			_, err := Unpack(tc)
+			_, err := Unpack2(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
 		})
 	}
