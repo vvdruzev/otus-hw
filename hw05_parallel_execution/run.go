32661b3b44a439af9bcc6426c64e14fe2793f7d4
Друзев Василий Викторович <vvdruze1@mts.ru>
2023-12-02 14:11:18 +0300

    HW5 is completed.

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,67 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	wg := sync.WaitGroup{}
+
+	counter := &Counter{
+		mu:           sync.Mutex{},
+		errCount:     0,
+		m:            int64(m),
+		tasksChannel: make(chan Task),
+		done:         make(chan struct{}, n),
+		err:          nil,
+	}
+
+	go func() {
+		defer func() {
+			close(counter.tasksChannel)
+			close(counter.done)
+		}()
+
+		for _, task := range tasks {
+			select {
+			case <-counter.done:
+				return
+			case counter.tasksChannel <- task:
+			}
+		}
+	}()
+	wg.Add(n)
+
+	for i := 0; i < n; i++ {
+		go func(i int) {
+			defer wg.Done()
+			for task := range counter.tasksChannel {
+				err := task()
+				if err != nil {
+					counter.little()
+				}
+			}
+		}(i)
+	}
+	wg.Wait()
+
+	return counter.err
+}
+
+type Counter struct {
+	mu           sync.Mutex
+	errCount     int64
+	m            int64
+	tasksChannel chan Task
+	done         chan struct{}
+	err          error
+	once         sync.Once
+}
+
+func (c *Counter) little() {
+	c.mu.Lock()
+	c.errCount++
+	if c.errCount >= c.m {
+		c.once.Do(func() {
+			c.done <- struct{}{}
+		})
+		c.err = ErrErrorsLimitExceeded
+	}
+	c.mu.Unlock()
 }
