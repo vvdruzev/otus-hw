e18f6c21a05e64bf4d8b9653877f73a635d17ff7
Друзев Василий Викторович <vvdruze1@mts.ru>
2023-12-02 20:42:51 +0300

    HW7 is completed.

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -11,5 +13,43 @@ var (
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
 	// Place your code here.
+	fileFrom, err := os.Open(fromPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// файл не найден
+			return err
+		}
+	}
+	defer fileFrom.Close()
+	fi, err := fileFrom.Stat()
+	if err != nil {
+		return err
+	}
+	if fi.Size() < limit+offset {
+		limit = 0
+	}
+	if offset > fi.Size() {
+		return err
+	}
+	if _, err := os.Stat(toPath); err == nil {
+		// path/to/whatever exists
+		os.Remove(toPath)
+	}
+	fileTo, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	defer fileTo.Close()
+	fileFrom.Seek(offset, io.SeekStart)
+
+	if limit == 0 {
+		b, _ := io.ReadAll(fileFrom)
+		fileTo.Write(b)
+	} else {
+		b := make([]byte, limit)
+		fileFrom.Read(b)
+		fileTo.Write(b)
+	}
 	return nil
 }
