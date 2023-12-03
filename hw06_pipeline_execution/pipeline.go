afd6525500380810fb820a544268d7266d6ad84f
Друзев Василий Викторович <vvdruze1@mts.ru>
2023-12-03 14:38:51 +0300

    HW6 is not completed.

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,7 @@
 package hw06pipelineexecution
 
+import "log"
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -10,5 +12,34 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	// Place your code here.
-	return nil
+	inn := make(chan interface{})
+	st := stages[0](inn)
+
+	go func() {
+		for _, stage := range stages[1:] {
+			st = stage(st)
+		}
+	}()
+	keeping := true
+	for keeping {
+		select {
+		case <-done:
+			close(inn)
+			keeping = false
+			log.Println("keeping = false")
+			break
+		case v, ok := <-in:
+			if !ok {
+				close(inn)
+				keeping = false
+				log.Println("keeping = false")
+
+				break
+			}
+			log.Printf("sdf %v\n", v)
+			inn <- v
+		}
+	}
+
+	return st
 }
